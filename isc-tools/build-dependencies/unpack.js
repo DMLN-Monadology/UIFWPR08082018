5519f97ec9f808c7b853ae25159ad1c637ad3974
Grace Huo <Grace.Huo@intersystems.com>
2018-02-08 14:34:09 -0500

    Merge the changes from operational report

--- isc-tools/build-dependencies/unpack.js
+++ isc-tools/build-dependencies/unpack.js
@@ -1,10 +1,22 @@
-var tar = require('../build-dependencies/tar');
-const argv = require('../build-dependencies/minimist')(process.argv.slice(2));
+var tar    = require( '../build-dependencies/tar' );
+const argv = require( '../build-dependencies/minimist' )( process.argv.slice( 2 ) );
+console.log( 'In Unpack.js' );
+console.log( 'Source:',  argv.source);
+console.log( 'Target:',  argv.target);
 
-tar.x(
-  {
-    file: argv.source,
-    C: argv.target
-  }
-);
-
+try {
+  console.log( 'Start to unpack!' );
+  tar.x(
+    {
+      file: argv.source,
+      C   : argv.target,
+      sync: true
+    }
+  );
+  console.log( 'End unpack!' );
+  process.exit( 0 );
+}
+catch ( error ) {
+  console.log( 'Unpack fail, reason: ', error );
+  process.exit( 1 );
+}
